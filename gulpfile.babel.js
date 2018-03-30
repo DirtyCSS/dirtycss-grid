2007b86a9a859ccc9ca671393d16ba9c4052d7c6
Stanislav <stanislavgolts@gmail.com>
2018-03-30 16:54:59 +0300

    ARCH: init

---
+++ gulpfile.babel.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import gulp from 'gulp';
+import del from 'del';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+gulp.task('styles', () => {
+  const AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+  ];
+
+  return gulp.src('src/**/*.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      precision: 10
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', () =>
+  del(['dist/*', 'example/dirtycss-grid.css'], {dot: true})
+);
