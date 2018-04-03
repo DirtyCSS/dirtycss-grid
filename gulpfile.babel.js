9220b867a13ec4cf7106e7f10751849a39a25b71
Stanislav Golts <stanislavgolts@gmail.com>
2018-04-03 23:11:05 +0300

    DEVELOP: add browser-sync, pug to gulp conf

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,12 +10,10 @@ const reload = browserSync.reload;
 
 gulp.task('styles', () => {
   const AUTOPREFIXER_BROWSERS = [
-    'ie >= 10',
-    'ie_mob >= 10',
+    'ie >= 11',
     'ff >= 30',
-    'chrome >= 34',
+    'chrome >= 21',
     'safari >= 7',
-    'opera >= 23',
     'ios >= 7',
     'android >= 4.4',
     'bb >= 10'
@@ -28,6 +26,25 @@ gulp.task('styles', () => {
     .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
     .pipe($.cssnano())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('pug', () =>
+  gulp.src('_pug/index.pug')
+    .pipe($.pug({
+       pretty: '\t'
+    }))
+    .pipe(gulp.dest('example/'))
+);
+
+gulp.task('serve', ['pug', 'styles'], () => {
+  browserSync({
+    notify: false,
+    server: ['example', 'dist'],
+    port: 3000
+  });
+
+  gulp.watch('src/**/*.scss', ['styles', reload]);
+  gulp.watch('_pug/**/*.pug', ['pug', reload]);
 });
 
 gulp.task('clean', () =>
